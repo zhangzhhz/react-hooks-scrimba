bf99bc2747422ea288cc8fedca640f4f8a0f9af9
Zhang ZH <zhangzhhz@gmail.com>
2020-09-19 20:49:31 +0000

    getting ready

---
+++ src/Playground.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import randomColor from 'randomcolor';
+
+export default function Playground() {
+  return (
+    <div className="playground">
+      Hello Worlds!
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Playground from './Playground'
+ 
+ReactDOM.render(<Playground />, document.getElementById('root'))
